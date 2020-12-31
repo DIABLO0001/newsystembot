3fd484aec0574d5b58c9ad4e8ef392d59566e941
DIABLO0001 <58942952+DIABLO0001@users.noreply.github.com>
2021-01-01 00:48:21 +0200

    asd

---
+++ .github/workflows/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "Nzk0MjgzNDU0MDA2OTUxOTU2.X-4kHA.eqEp00tonZDKm7eGmI1BE5HetLU", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "$",
+  devs: ["578719620412735490"] ///تعديل مهم ايدي حسابك
+};
